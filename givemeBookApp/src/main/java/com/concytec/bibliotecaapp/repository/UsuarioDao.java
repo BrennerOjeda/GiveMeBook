804e5dfd79770fe42f419d61f4a5226d9428eb8c
goropezaq <greis.oropeza@gmail.com>
2014-07-30 11:07:47 -0500

    Agregando las interfaces de cada Entidad de la BD

---
+++ givemeBookApp/src/main/java/com/concytec/bibliotecaapp/repository/UsuarioDao.java
@@ -0,0 +1,16 @@
+package com.concytec.bibliotecaapp.repository;
+
+import java.util.List;
+import com.concytec.bibliotecaapp.domain.Usuario;
+
+public interface UsuarioDao {
+	public void add(Usuario usuario);
+	public void edit(Usuario usuario);
+	public void delete(Usuario usuario);
+	public Usuario getUsuario(String docIdeUsu);
+	public List<Usuario> getAllUsuarios();
+	public Usuario loginUsuario (String docIdeUsu, String password);
+	//public List<Foto> getUsuarioFotos(Usuario usuario);
+	//public List<String> getLastNews (Usuario usuario);
+
+}
